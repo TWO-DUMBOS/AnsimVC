561977a4878955e8835a74e5202208b805c4c3f0
jihyun132 <jihyun.q.s@gmail.com>
2026-09-16 01:07:02 +0900

    feat: add initial page routes

---
+++ app/diagnosis/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function DiagnosisPage() {
+  return (
+	<div>
+		<div>AI 자격 진단</div>
+
+		<Link href="/proof">
+			다음
+		</Link>
+	</div>
+  )
+}
